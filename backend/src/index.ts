e54ac10055d832560164789e1b44ceec8f9ed4d2
unknown <michaelstephanomtambo@gmail.com>
2024-10-21 15:46:38 +0300

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 
-import authRoute from "../routes/auth.route";
-import productRoute from "../routes/product.route";
+import authRoute from "./routes/auth.route";
+import productRoute from "./routes/product.route";
 
-const PORT= process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -12,11 +12,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    
-});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
