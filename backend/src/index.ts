f08d3da6c7ce0681bcae2e883c73502278b667fb
unknown <michaelstephanomtambo@gmail.com>
2024-10-21 12:29:12 +0300

    adding json() meddleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import productRoute from "../routes/product.route";
 const PORT= process.env.PORT || 3000;
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
